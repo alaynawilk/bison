00d12ef4fe7b1281e7e109c2ba86202e5d7c8bc6
alaynawilk <alaynawilkening@gmail.com>
2026-03-25 17:59:54 -0400

    title test

--- sketch.js
+++ sketch.js
@@ -3,5 +3,10 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background('#303030');
+  fill('white');
+  textSize(36)
+  textAlign(CENTER, CENTER);
+  text('How much did IU spend', 200, 100);
+  text('to bring back the bison?', 200, 145);
 }
